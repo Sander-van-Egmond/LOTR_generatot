4325a831084a714f83c71b8f31d41c273e4992e2
Sander <sander.vanegmond@outlook.com>
2018-03-23 14:00:26 +0100

    scenario stringarrays added

--- app/src/main/java/com/example/sander/lotrscenariogenerator/MainActivity.java
+++ app/src/main/java/com/example/sander/lotrscenariogenerator/MainActivity.java
@@ -1,12 +1,15 @@
 package com.example.sander.lotrscenariogenerator;
 
 import android.content.Intent;
+import android.content.res.Resources;
+import android.content.res.TypedArray;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Toast;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.Random;
@@ -51,25 +54,41 @@ public class MainActivity extends AppCompatActivity {
                 chosenDecks = new ArrayList<>(monsterDecks.subList(0,3));
             }
 
-            scenario.putStringArrayListExtra("deck", chosenDecks);
+
+            scenario.putStringArrayListExtra("scenario_1",scenarioPicker(chosenDecks,1));
+
+
             startActivity(scenario);
         }
     }
-    //    private String scenarioPicker(ArrayList<String> decks, int scenarioNumber){
-//        ArrayList<String> options = new ArrayList<>();
-//        Class<R.array> res = R.array.class;
-//        Field field;
-//        for (String deck : decks) {
-//            try {
-//                field = res.getField(deck + "_" + Integer.toString(scenarioNumber));
-//                options.addAll(Arrays.asList(context.getStringArray(field.getInt(null))));
-//            }catch (Exception e){
-//                Toast.makeText(this,
-//                        "No such field",
-//                        Toast.LENGTH_SHORT).show();
-//            }
-//        }
-//        Collections.shuffle(options);
-//        return options.get(0);
-//    }
+
+
+    private ArrayList<String> scenarioPicker(ArrayList<String> decks, int scenarioNumber){
+
+        ArrayList<ArrayList<String>> options = new ArrayList<ArrayList<String>>();
+
+        for (String deck : decks) {
+
+            int holderId = getResources().getIdentifier(deck + "_" + Integer.toString(scenarioNumber),
+                    "array",
+                    this.getPackageName());
+            TypedArray ta = getResources().obtainTypedArray(holderId);
+
+            int n = ta.length();
+            for (int i = 0; i < n; ++i) {
+                int id = ta.getResourceId(i, 0);
+                if (id > 0) {
+                    options.add(new ArrayList<>(Arrays.asList(getResources().getStringArray(id))));
+                } else {
+                    // something wrong with the XML
+                }
+            }
+            ta.recycle(); // Important!
+        }
+        Collections.shuffle(options);
+        return options.get(0);
+
+    }
+
+
 }
--- app/src/main/java/com/example/sander/lotrscenariogenerator/ScenarioActivity.java
+++ app/src/main/java/com/example/sander/lotrscenariogenerator/ScenarioActivity.java
@@ -7,6 +7,7 @@ import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -16,20 +17,23 @@ public class ScenarioActivity extends AppCompatActivity {
     ListView listView ;
     Integer scenario;
     Button nextButton, previousButton;
+    TextView setupText;
+    Intent caller;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scenario);
         //Retrieve information
-        Intent caller = getIntent();
-        monsterdecks = caller.getStringArrayListExtra("deck");
+        caller = getIntent();
+        monsterdecks = caller.getStringArrayListExtra("scenario_1");
 
         //set variables
-        scenario = 0;
+        scenario = 1;
         nextButton = findViewById(R.id.nextscenario_button);
         previousButton = findViewById(R.id.previousscenario_button);
         previousButton.setEnabled(false);
+        setupText = findViewById(R.id.setup_text);
 
 
         listView = findViewById(R.id.list);
@@ -45,19 +49,20 @@ public class ScenarioActivity extends AppCompatActivity {
     }
 
     public void nextScenario(View v){
-        if (scenario ==0){
+        if (scenario ==1){
             previousButton.setEnabled(true);
-        }else if(scenario == 4){
+        }else if(scenario == 5){
             nextButton.setEnabled(false);
         }
         scenario += 1;
+        setupText.setText(caller.getStringExtra("scenario_"+Integer.toString(scenario)));
     }
 
 
     public void previousScenario(View v){
-        if (scenario==5){
+        if (scenario==6){
             nextButton.setEnabled(true);
-        }else if (scenario == 1){
+        }else if (scenario == 2){
             previousButton.setEnabled(false);
         }
         scenario -= 1;
